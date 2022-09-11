2e1dd2dd8e764ae77322ada7cbd8947ba38c408b
OluwaseyiOJ6 <richjuve@gmail.com>
2022-09-11 13:42:13 -0700

    if_else

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	char lower;
 
-	for (lower = 'a'; lower <= 'z', lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 		putchar(lower);
-	for (lower = 'A'; lower <= 'Z', lower++)
+	for (lower = 'A'; lower <= 'Z'; lower++)
 		putchar(lower);
 	putchar('\n');
 	return (0);
