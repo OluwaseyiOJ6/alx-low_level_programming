8d0fdb035a9b2f7571b2157d44cd687c0cbcbb3b
OluwaseyiOJ6 <richjuve@gmail.com>
2022-09-14 13:25:08 -0700

    functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - this program checks wether a character is from the English alphabet.
+ * _isalpha - this program checks wether a character
+ * is from the English alphabet.
  * @c: input parameter
  * Return: 1 for English Character 0 if the character is not English
  */
