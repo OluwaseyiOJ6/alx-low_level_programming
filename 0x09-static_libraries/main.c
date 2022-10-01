3803e843d7401d2bd4bce002aaa2d3d6951b9939
OluwaseyiOJ6 <richjuve@gmail.com>
2022-09-30 17:29:50 -0700

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best programmeer\"\n\t- Seun Ade");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
