ac072938fd5dcb6a231312e4db8d843f3e02febe
OluwaseyiOJ6 <richjuve@gmail.com>
2022-10-24 14:37:57 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_nodeint - A function that adds a new nod at the begining of a list
- * @head: A pointer to list_t structure
- * @n: An integer data to put into the new node
- * Return: the address of the new element, or NULL on failure
+ * pop_listint - A function that deletes the head node of a list
+ * and returns the head node's data n.
+ * @head: A pointer to listint_t structure.
+ * Return: The head node's datafrom deleted element
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+int pop_listint(listint_t **head)
 {
 	listint_t *new_node;
+	int n;
 
-	new_node = *head;
-	*head = malloc(sizeof(listint_t));
-	if (!*head)
-		return (NULL);
-	(*head)->n = n;
-	(*head)->next = new_node;
-	return (*head);
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	new_node = (*head)->next;
+	free(*head);
+	*head = new_node;
+	return (n);
 }
